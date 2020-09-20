8f7e7dbf0a69bf3d1d7c5dcce2961b916fa3608f
Vinicio Alves <vinicio.gudu@gmail.com>
2020-09-20 15:07:37 -0300

    first commit

---
+++ js/dashboard.js
@@ -0,0 +1,8 @@
+function esconderMenu() {
+    var x = document.getElementById("dashboard");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
